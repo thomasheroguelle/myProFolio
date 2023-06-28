f3b4720a357a6ec717957121dafe954d9fb36e6a
HEROGUELLE Thomas <thomas.heroguelle@renault.com>
2023-06-28 11:26:41 +0200

    ajustements, ajout description projects

--- src/mock/mock.ts
+++ src/mock/mock.ts
@@ -15,7 +15,7 @@ export const PROJETS: IProjets[] = [
   {
     id: 1,
     title: "TODO LISTE",
-    description: "loremDZFOUBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+    description: "Todoliste is a web application developed with Angular for efficient management of daily tasks. With its home page, urgent and non-urgent task creation functionality, task visualization page and history page, Todoliste offers a user-friendly, responsive interface. Thanks to Angular, the application offers a fluid and dynamic user experience, making it easy to organize and manage daily tasks.",
     imagesArray: [
       "assets/todoliste/HOME.png",
       "assets/todoliste/CREATE_TASK.png",
@@ -29,7 +29,7 @@ export const PROJETS: IProjets[] = [
   {
     id: 2,
     title: "Travel Note Book",
-    description: "loremDZFOUBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+    description: "The travel application I developed with Angular offers an immersive and aesthetically pleasing experience thanks to its clean design. It gives users easy access to beautiful images of different countries, while offering detailed information on the pros and cons of each destination. What's more, the application is fully responsive and offers practical features such as saving favorite destinations, travel tips and experience sharing. Thanks to the Angular technology stack, the application guarantees a fluid user experience, with optimal data management and smooth screen transitions.",
     imagesArray: [
       "assets/travelnotebook/HOME.png",
       "assets/travelnotebook/ALBANIE.png",
@@ -41,7 +41,7 @@ export const PROJETS: IProjets[] = [
   {
     id: 3,
     title: "E-SHOP B.A.S.E.",
-    description: "loremDZFOUBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+    description: "The eShop B.A.S.E project is an e-commerce platform developed with Angular, featuring a clean design for a pleasant user experience. With its six main pages, including the home page, item list, item detail page, categories, shopping cart and checkout page, the site enables users to navigate easily and complete their purchases with ease. Thanks to the Angular technology stack, the project guarantees a responsive and fluid experience, offering an intuitive interface and optimal functionality for an optimized online shopping experience.",
     imagesArray: [
       "assets/base/HOMEPAGE.png",
     ],
